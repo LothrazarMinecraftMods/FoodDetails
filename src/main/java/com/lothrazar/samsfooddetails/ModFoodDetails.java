d7343c6d5a9c3540bf76d3f6f11dcf2410ff4047
Sam <samson.bassett@gmail.com>
2016-08-15 23:02:45 -0700

    update to 1.10

--- src/main/java/com/lothrazar/samsfooddetails/ModFoodDetails.java
+++ src/main/java/com/lothrazar/samsfooddetails/ModFoodDetails.java
@@ -1,5 +1,4 @@
 package com.lothrazar.samsfooddetails;
-
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
@@ -11,30 +10,23 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod(modid = ModFoodDetails.MODID, useMetadata = true, updateJSON = "https://raw.githubusercontent.com/LothrazarMinecraftMods/FoodDetails/master/update.json")
-public class ModFoodDetails{
-
-	public static final String MODID = "samsfooddetails";
-	@Instance(value = ModFoodDetails.MODID)
-	public static ModFoodDetails instance;
-
-	@EventHandler
-	public void onPreInit(FMLPreInitializationEvent event){
-
-		MinecraftForge.EVENT_BUS.register(instance);
-	}
-
-	@SubscribeEvent
-	public void onItemTooltipEvent(ItemTooltipEvent event){
-
-		// https://www.reddit.com/r/minecraftsuggestions/comments/3brh7v/when_hovering_over_a_food_it_shows_how_many_food/
-		ItemStack itemStack = event.getItemStack();
-		if(itemStack != null && itemStack.getItem() instanceof ItemFood){
-			ItemFood food = (ItemFood) itemStack.getItem();
-
-			int hunger = food.getHealAmount(itemStack);
-			float satur = food.getSaturationModifier(itemStack);
-
-			event.getToolTip().add(hunger + " (" + satur + ")");
-		}
-	}
+public class ModFoodDetails {
+  public static final String MODID = "samsfooddetails";
+  @Instance(value = ModFoodDetails.MODID)
+  public static ModFoodDetails instance;
+  @EventHandler
+  public void onPreInit(FMLPreInitializationEvent event) {
+    MinecraftForge.EVENT_BUS.register(instance);
+  }
+  @SubscribeEvent
+  public void onItemTooltipEvent(ItemTooltipEvent event) {
+    // https://www.reddit.com/r/minecraftsuggestions/comments/3brh7v/when_hovering_over_a_food_it_shows_how_many_food/
+    ItemStack itemStack = event.getItemStack();
+    if (itemStack != null && itemStack.getItem() instanceof ItemFood) {
+      ItemFood food = (ItemFood) itemStack.getItem();
+      int hunger = food.getHealAmount(itemStack);
+      float satur = food.getSaturationModifier(itemStack);
+      event.getToolTip().add(hunger + " (" + satur + ")");
+    }
+  }
 }
